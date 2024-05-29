e7d01b5a8e7a21505bafdcecbdb5e258ad0731de
AlexProg34-G <alexgur3@gmail.com>
2024-05-29 18:24:56 +0300

    main update

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
 
   return (
     <Container>
-      <Header setShowSideBar={setShowSideBar} page={page} setPage{setPage}/>
+      <Header setShowSideBar={setShowSideBar} page={page} setPage={setPage}/>
       <Body
         showSideBar={showSideBar}
         setShowSideBar={setShowSideBar}
