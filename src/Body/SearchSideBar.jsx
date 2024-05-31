b1a0a476f842d8f29806c5f3068472af870b0a4c
AlexProg34-G <alexgur3@gmail.com>
2024-05-31 12:32:21 +0300

    updfate branch

--- src/Body/SearchSideBar.jsx
+++ src/Body/SearchSideBar.jsx
@@ -4,6 +4,7 @@ import SearchForm from "./SearchForm";
 function SearchSideBar({ showSideBar, setShowSideBar }) {
   const handleClose = () => setShowSideBar(false);
 
+  
   return (
     <Offcanvas show={showSideBar} onHide={handleClose} placement="end">
       <Offcanvas.Header closeButton>
